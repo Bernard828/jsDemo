4ed080c1c630ca1da4317cd17bcba4cbf320a72e
Bernard828 <bernard.lackey1@gmail.com>
2020-08-18 10:39:00 -0400

    initial commit

--- app.js
+++ app.js
@@ -1,10 +1,68 @@
 // Make h1 in header have different text
+
+// const h1Text = document.getElementsByTagName('h1')[0]
+const h1Text = document.querySelector('header1')
+h1Text.innertext = "Java needs more coffee to unhack this hacker |_|]!!!"
+
+
+
+
+
+
 // Make nav ul li elements get larger when you hover over them
+
+const liList = document.querySelectorAll("#li")
+liList.addEventListener("hover", function () {
+    liList.style.backgroundColor = "yellow"
+    liList.style.fontSize= "45px"
+})
+
+
+
+
 // Add another element to the main element
+let elementP = document.createElement('main');
+elementP.innerText = "this is a test"
+
+
+
+
 // Dynamically change the color of the background on the footer when the user hovers over it
+
+
+
 // Change the font size of the paragraphs in the main element when the user clicks them
+
+document.getElementsById('p')[0] = function () {
+    this.style.backgroundColor = '5%';
+
+}
+
 // Change the text in the footer when the user clicks it
+
+
+
+
+
+
+
 // Remove the h1 element in the header
+
+
+
 // Put something in the place of the h1 in the header
+document.getElementById(header2)
+
 // Change the text in one of the paragraph tags in the main element
+document.getElementById()
+const paragraph = document.getElementById("p")
+paragraph.innerHTML = "Test"
+
 // Change the size of the header element to 200px using grid syntax
+
+
+    // GridLayout  = new GridLayout(0, 1);
+    // comp style gridTemplateColumns = "200px"
+    // function GridLayout() {
+    // const head = document.getElementById("head")\
+
